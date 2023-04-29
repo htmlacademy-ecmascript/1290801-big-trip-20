6493466370a05488bd03aa236cb79335fd1af27a
Alexey Chernykh <alexeyshockman@gmail.com>
2023-04-29 19:56:52 +0300

    components cut, presenter added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import TripInfoView from "./view/trip-info-view";
+import TripInfoView from './view/trip-info-view';
 import FilterView from './view/filter-view.js';
-import ListPresenter from "./presenter/list-presenter";
+import ListPresenter from './presenter/list-presenter';
 import {render} from './render.js';
-import {RenderPosition} from "./render.js";
+import {RenderPosition} from './render.js';
 
 
 const siteMainElement = document.querySelector('.page-body');
 const siteHeaderFilterElement = siteMainElement.querySelector('.trip-controls__filters');
 const siteHeaderInfoElement = siteMainElement.querySelector('.trip-main');
-const siteBodyElement = siteMainElement.querySelector('.trip-events')
+const siteBodyElement = siteMainElement.querySelector('.trip-events');
 
 render(new TripInfoView(), siteHeaderInfoElement, RenderPosition.AFTERBEGIN);
 render(new FilterView(), siteHeaderFilterElement);
