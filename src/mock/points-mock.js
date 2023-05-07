d333e6da0cc6ccc94a71dfd4b8b0dd32675719ca
Alexey Chernykh <alexeyshockman@gmail.com>
2023-05-07 22:26:21 +0300

    mocks for points, offers, destinations have been created and are being generated. They are connected. The result is exported through the model.

--- src/mock/points-mock.js
+++ src/mock/points-mock.js
@@ -1,50 +1,38 @@
-import {getRandomArrayElement, humanizeDate, timeDifference, getRandomInt, getRandomTimeFrom} from '../utils';
+import {
+  getRandomArrayElement,
+  getRandomInt,
+  getRandomTime
+} from '../utils';
 import {POINTS_TYPE} from '../const';
+import {getOffers} from './offers-mock';
+import {getDestination} from './destination-mock';
 
-const pointDestination = ['Moscow', 'Texas', 'London', 'Tokyo', 'New-York'];
-
-const timeFrom = "2019-07-11T12:00:00.000Z";
-const timeTo = "2019-07-11T13:00:00.000Z";
-
-
-//Задачи;
-// переработать время. Тут его хранить в двух переменных from и to;
-// во VIEW уже делать со временем все необходимые преобразования;
-// подготовить для этого в utils нужные функции;
-// соединить offers и points;
-// Настроить VIEW для точек и сделать VIEW для формы редактирования/создания. Сделать пустую точку для формы создания;
-// Перенастроить версии в package.json;
-
-//готово
-// Документация utils;
-// Переделать функцию определения разницы во времени;
-// Сделать функции для генерации времени;
-
-
-
-
+// const pointDestination = ['Moscow', 'Texas', 'London', 'Tokyo', 'New-York'];
+const pointDestination = ['b8bbe9a6-9fde-4713-930b-8105ed8decbb', '0ac9df44-6b6d-4233-8d29-3e227a3e470f', '115c1f07-f50e-4811-8b30-6e386023d457', '1ad0d08f-7172-4c3b-9fa1-5da21ea95bb2', '8e1901ba-1e26-43fa-b740-95a8286ce345'];
 
 function getRandomPoint() {
 
-  //генерация случайного времени для timeFrom
-
-  //увеленичение timeFrom на случайное количество минут
-
+  const offersCol = getRandomInt(3);
+  const type = getRandomArrayElement(POINTS_TYPE);
+  const offersThisType = getOffers(type).offers;
+  const dateFrom = getRandomTime();
+  const dateTo = getRandomTime(dateFrom);
+  const destination = getRandomArrayElement(pointDestination);
+  const offers = [];
+  for (let i = 0; i < offersCol; i++){
+    offers.push(getRandomArrayElement(offersThisType).id);
+  }
 
   const mockPoints = [
     {
-      "basePrice": getRandomInt(3000),
-      "timeFrom": humanizeDate(timeFrom, true),
-      "timeTo": humanizeDate(timeTo, true),
-      "date" : humanizeDate(timeFrom),
-      "duration" : timeDifference(timeFrom, timeTo),
-      "destination": getRandomArrayElement(pointDestination),
-      "isFavorite": getRandomInt(2),
-      "offers": [
-        "some offer one",
-        "some offer two"
-      ],
-      "type": getRandomArrayElement(POINTS_TYPE)
+      'basePrice': getRandomInt(3000),
+      'dateFrom': dateFrom,
+      'dateTo': dateTo,
+      'destination': getDestination(destination),
+      'isFavorite': getRandomInt(2),
+      'offers': offers,
+      'allOffersThisType' : offersThisType,
+      'type': type
     },
   ];
   return getRandomArrayElement(mockPoints);
@@ -54,14 +42,14 @@ export {getRandomPoint};
 
 // From server:
 // {
-//   "id": "f4b62099-293f-4c3d-a702-94eec4a2808c",
-//   "base_price": 1100,
-//   "date_from": "2019-07-10T22:55:56.845Z",
-//   "date_to": "2019-07-11T11:22:13.375Z",
-//   "destination": "bfa5cb75-a1fe-4b77-a83c-0e528e910e04",
-//   "is_favorite": false,
-//   "offers": [
-//   "b4c3e4e6-9053-42ce-b747-e281314baa31"
+//   'id': 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+//   'base_price': 1100,
+//   'date_from': '2019-07-10T22:55:56.845Z',
+//   'date_to': '2019-07-11T11:22:13.375Z',
+//   'destination': 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+//   'is_favorite': false,
+//   'offers': [
+//   'b4c3e4e6-9053-42ce-b747-e281314baa31'
 // ],
-//   "type": "taxi"
+//   'type': 'taxi'
 // }
