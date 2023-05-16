61793955c20692cc34b1076fab65f8dd4844e62c
Alexey Chernykh <alexeyshockman@gmail.com>
2023-05-16 23:44:33 +0300

    refactor: added private fields and getters

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -6,24 +6,29 @@ import EditingPointView from '../view/editing-point-view';
 
 
 export default class ListPresenter {
-  listComponent = new ListView();
-  sortComponent = new SortView();
+  #listContainer = null;
+  #pointsModel = null;
+  #destinationsModel = null
+
+
+  #listComponent = new ListView();
+  #sortComponent = new SortView();
 
   constructor({listContainer, pointsModel, destinationsModel}) {
-    this.listContainer = listContainer;
-    this.pointsModel = pointsModel;
-    this.destinationsModel = destinationsModel;
+    this.#listContainer = listContainer;
+    this.#pointsModel = pointsModel;
+    this.#destinationsModel = destinationsModel;
   }
 
   init() {
-    this.listPoints = this.pointsModel.getPoints();
-    this.allDestinations = this.destinationsModel.getAllDestinations();
+    this.listPoints = this.#pointsModel.points;
+    this.allDestinations = this.#destinationsModel.allDestinations;
 
-    render(this.sortComponent, this.listContainer);
-    render(this.listComponent, this.listContainer);
-    render(new EditingPointView({point: this.listPoints[0]}, this.allDestinations), this.listComponent.element);
+    render(this.#sortComponent, this.#listContainer);
+    render(this.#listComponent, this.#listContainer);
+    render(new EditingPointView({point: this.listPoints[0]}, this.allDestinations), this.#listComponent.element);
     for (let i = 1; i < this.listPoints.length; i++) {
-      render(new PointView({point: this.listPoints[i]}), this.listComponent.element);
+      render(new PointView({point: this.listPoints[i]}), this.#listComponent.element);
     }
 
   }
