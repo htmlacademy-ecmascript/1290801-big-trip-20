76cd9583172f7823e114b3d2b8d30180581557eb
Alexey Chernykh <alexeyshockman@gmail.com>
2023-05-30 01:39:18 +0300

    WIP edit-point-view data binding

--- src/const.js
+++ src/const.js
@@ -1,5 +1,5 @@
 const POINTS_TYPE = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-const MOCK_POINT_COUNT = 10;
+const MOCK_POINT_COUNT = 3;
 
 
 const SORT_TYPE = {
