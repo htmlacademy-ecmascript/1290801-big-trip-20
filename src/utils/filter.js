793495e84e595a0cf211e45848e258c932e3b526
Alexey Chernykh <alexeyshockman@gmail.com>
2023-06-04 19:02:31 +0300

    filters completed

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,12 @@
 import {FILTER_TYPE} from '../const';
+import {checkFuture, checkPresent, checkPast} from './time';
+
 
 const filter = {
   [FILTER_TYPE.EVERYTHING]: (points) => points,
-  [FILTER_TYPE.FUTURE]: (points) => points,
-  [FILTER_TYPE.PRESENT]: (points) => points,
-  [FILTER_TYPE.PAST]: (points) => points.filter((point) => point.isFavorite === null),
+  [FILTER_TYPE.FUTURE]: (points) => points.filter((point) => checkFuture(point.dateFrom)),
+  [FILTER_TYPE.PRESENT]: (points) => points.filter((point) => checkPresent(point.dateFrom, point.dateTo)),
+  [FILTER_TYPE.PAST]: (points) => points.filter((point) => checkPast(point.dateTo)),
   [FILTER_TYPE.FAVORITE]: (points) => points.filter((point) => point.isFavorite),
 };
 
