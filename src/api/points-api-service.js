bdf18833f23c30e21c817ee2745c7d37ece1c629
Alexey Chernykh <alexeyshockman@gmail.com>
2023-06-18 00:04:21 +0300

    added deleting and adding points through the server

--- src/api/points-api-service.js
+++ src/api/points-api-service.js
@@ -36,6 +36,26 @@ export default class PointsApiService extends ApiService {
     return await ApiService.parseResponse(response);
   }
 
+  async addPoint(point) {
+    const response = await this._load({
+      url: 'points',
+      method: Method.POST,
+      body: JSON.stringify(this.#adaptToServer(point)),
+      headers: new Headers({'Content-Type': 'application/json'})
+    });
+
+    return await ApiService.parseResponse(response);
+  }
+
+  async deletePoint(point) {
+    const response = await this._load({
+      url: `points/${point.id}`,
+      method: Method.DELETE
+    });
+
+    return response;
+  }
+
   #adaptToServer(point) {
     const adaptedPoint = {
       ...point,
